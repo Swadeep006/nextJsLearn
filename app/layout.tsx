40e25e49bb576f7bfae4d15d821f5338fab88c9d
Swadeep <dhondiswadeepdsp6@gmail.com>
2026-02-09 11:36:25 +0530

    THE END

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Prepdha.ai',
+    default: 'Prepdha Dashboard',
+  },
+  description: 'An EdTech startup',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
